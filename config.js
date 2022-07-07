36a28ee200afbaeeae40bcc1370f9e9ee6976bf7
Aliaksandra Khvaliuk <a.hvaluk@gmail.com>
2022-07-07 11:09:31 +0200

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://127.0.0.1:27017/conFusion'
+    'mongoUrl': 'mongodb://127.0.0.1:27017/conFusion',
+    'facebook': {
+        clientId: '724708598802061',
+        clientSecret: '257b349ec22b54e981fdf786ee7814c6'
+    }
 }
