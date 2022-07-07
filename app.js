2ef689c09f388330b96dfaa4ef1b7081320464c5
Aliaksandra Khvaliuk <a.hvaluk@gmail.com>
2022-07-07 14:36:01 +0200

    Assignment 4: Backend as a Service

--- app.js
+++ app.js
@@ -15,10 +15,14 @@ var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var favoriteRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
+const Leaders = require('./models/leaders');
+const Promotions = require('./models/promotions');
+const Favorites = require('./models/favorite');
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url);
@@ -60,6 +64,8 @@ app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
